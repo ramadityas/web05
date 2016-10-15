22d7f736f2057528aa668875cbb8cdda79469196
ramadityas <ramaditya.id@gmail.com>
2016-10-16 00:30:38 +0700

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -1,11 +1,26 @@
 <html>
 <head>
 <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'">
+        <script type="text/javascript" charset="utf-8" src="cordova-1.5.0.js"></script>      
+        <script type="text/javascript" charset="utf-8">
+
+            function onLoad()
+            {
+                  document.addEventListener("deviceready", onDeviceReady, true);
+            }
+
+            function exitFromApp()
+             {
+                navigator.app.exitApp();
+             }
+
+        </script>
 </head>
-<body style="margin: 0;">
+<body style="margin: 0;" onload="onLoad();">
 <br><br>
 <center><img border="0" src="icon.png" /></center><br>
-<center><h3><a href="index.html" style="text-decoration:none"><b>Refresh</b></a></h3></center>
+<center><h3><a onclick="exitFromApp()" style="text-decoration:none"><b>Keluar Aplikasi</b></a></h3></center><br>
+<center><h3><a href="index.html" style="text-decoration:none"><b>Masuk</b></a></h3></center>
 
 <script type="text/javascript">
 function checkNetConnection(){
