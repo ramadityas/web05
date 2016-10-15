a41d4345857d005c2a63cb3c495ab5270c8952e9
ramadityas <ramaditya.id@gmail.com>
2016-10-15 23:08:36 +0700

    Create refresh.php

---
+++ refresh.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'">
+</head>
+<body style="margin: 0;">
+<br><br>
+<center><img border="0" src="icon.png" /></center><br>
+<center><h3><a href="index.html" style="text-decoration:none"><b>Refresh</b></a></h3></center>
+
+<script type="text/javascript">
+function checkNetConnection(){
+ var xhr = new XMLHttpRequest();
+ var file = "http://rama.xp3.biz/alkitab/load.png";
+ var r = Math.round(Math.random() * 10000);
+ xhr.open('HEAD', file + "?subins=" + r, false);
+ try {
+  xhr.send();
+  if (xhr.status >= 200 && xhr.status < 304) {
+   return true;
+  } else {
+   return false;
+  }
+ } catch (e) {
+  return false;
+ }
+}
+if(checkNetConnection()) {
+    window.location = "http://rama.xp3.biz/alkitab/";
+} else {
+    alert("Tidak ada koneksi internet");
+    window.location.history.go(-2)
+}
+</script>
+</body>
+</html>
