418933d1bc202231291c6328726e615bac0fcbe8
ramadityas <ramaditya.id@gmail.com>
2016-10-15 15:35:46 +0700

    Update check.js

--- check.js
+++ check.js
@@ -1,18 +1,17 @@
-function checkConnection() {
-    var networkState = navigator.connection.type;
- 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
- 
-    alert('Connection type: ' + states[networkState]);
+public static boolean hasActiveInternetConnection(Context context) {
+    if (isNetworkAvailable(context)) {
+        try {
+            HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+            urlc.setRequestProperty("User-Agent", "Test");
+            urlc.setRequestProperty("Connection", "close");
+            urlc.setConnectTimeout(1500); 
+            urlc.connect();
+            return (urlc.getResponseCode() == 200);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error checking internet connection", e);
+        }
+    } else {
+        Log.d(LOG_TAG, "No network available!");
+    }
+    return false;
 }
- 
-checkConnection();
-return;
